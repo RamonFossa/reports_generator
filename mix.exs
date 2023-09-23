1a1b678059eb4fc3b98e41513414e9c7a63ed6cd
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 18:16:09 -0300

    initial commit

---
+++ mix.exs
@@ -0,0 +1,25 @@
+defmodule ReportsGenerator.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :reports_generator,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
