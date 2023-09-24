297d69816ef5c9e45ae032141d27d4382cf737cb
RamonFossa <ramon.fossa@gmail.com>
2023-09-23 22:49:24 -0300

    feat: Reading file with stream function and parsing values right

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,11 +1,18 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.reduce(%{}, fn line, report ->
+      [id, _food_name, price] = parse_line(line)
+      last_price = Map.get(report, id, 0)
+      Map.put(report, id, price + last_price)
+    end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 end
