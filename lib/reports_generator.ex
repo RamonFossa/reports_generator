a27f2e24925b0ae14eb2027de535f3eb59d9f8e2
RamonFossa <ramon.fossa@gmail.com>
2023-10-12 20:06:40 -0300

    feat: New function to return higher cost

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,7 +4,19 @@ defmodule ReportsGenerator do
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(%{}, fn line, report -> sum_values(line, report) end)
+    |> Enum.reduce(%{"users" => %{}, "foods" => %{}}, fn line, report ->
+      sum_values(line, report)
+    end)
+  end
+
+  def teste(filename) do
+    filename
+    |> build()
+    |> fetch_higher_cost()
+  end
+
+  defp fetch_higher_cost(report) do
+    Enum.max_by(report, fn {_id, price} -> price end)
   end
 
   defp sum_values([id, _food_name, price], report) do
