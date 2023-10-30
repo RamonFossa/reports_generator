6f67c6a3a5d2d06710c3cfb74d0b8dbc3f237dc0
Ramon Fossa Chiesi <ramon.fossa@triibo.com.br>
2023-10-29 23:31:50 -0300

    feat: Program now builds the number of times that a food was bought

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -15,12 +15,24 @@ defmodule ReportsGenerator do
     |> fetch_higher_cost()
   end
 
-  defp fetch_higher_cost(report) do
+  def fetch_higher_cost(report) do
     Enum.max_by(report, fn {_id, price} -> price end)
   end
 
-  defp sum_values([id, _food_name, price], report) do
-    last_price = Map.get(report, id, 0)
-    Map.put(report, id, price + last_price)
+  defp sum_values([id, food_name, price], report) do
+    {_ok, users} = Map.fetch(report, "users")
+    {_ok, foods} = Map.fetch(report, "foods")
+
+    last_price = Map.get(users, id, 0)
+    users_data = Map.put(users, id, price + last_price)
+
+    food_count = Map.get(foods, food_name, 0)
+    foods_data = Map.put(foods, food_name, food_count + 1)
+
+    # report
+    # |> Map.replace("users", users_data)
+    # |> Map.replace("foods", foods_data)
+
+    %{report | "users" => users_data, "foods" => foods_data}
   end
 end
